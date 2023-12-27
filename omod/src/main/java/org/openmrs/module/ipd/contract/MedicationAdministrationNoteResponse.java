cadcc349167e9743aa0c3d4ea1a22fb25186e953
Kavitha S <kavitha.s@thoughtworks.com>
2023-12-27 16:51:37 +0530

    Kavitha | refactored annotation to medicationAdministrationNote

--- omod/src/main/java/org/openmrs/module/ipd/contract/MedicationAdministrationNoteResponse.java
+++ omod/src/main/java/org/openmrs/module/ipd/contract/MedicationAdministrationNoteResponse.java
@@ -2,7 +2,7 @@ package org.openmrs.module.ipd.contract;
 
 import lombok.*;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
-import org.openmrs.module.ipd.api.model.Annotation;
+import org.openmrs.module.ipd.api.model.MedicationAdministrationNote;
 import org.openmrs.module.webservices.rest.web.ConversionUtil;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
 
@@ -20,12 +20,12 @@ public class MedicationAdministrationNoteResponse {
     private Date recordedTime;
     private String text;
 
-    public static MedicationAdministrationNoteResponse createFrom(Annotation openmrsAnnotation) {
+    public static MedicationAdministrationNoteResponse createFrom(MedicationAdministrationNote openmrsObject) {
         return MedicationAdministrationNoteResponse.builder()
-                .uuid(openmrsAnnotation.getUuid())
-                .author(ConversionUtil.convertToRepresentation(openmrsAnnotation.getAuthor(), Representation.REF))
-                .recordedTime(openmrsAnnotation.getRecordedTime())
-                .text(openmrsAnnotation.getText())
+                .uuid(openmrsObject.getUuid())
+                .author(ConversionUtil.convertToRepresentation(openmrsObject.getAuthor(), Representation.REF))
+                .recordedTime(openmrsObject.getRecordedTime())
+                .text(openmrsObject.getText())
                 .build();
     }
 }
