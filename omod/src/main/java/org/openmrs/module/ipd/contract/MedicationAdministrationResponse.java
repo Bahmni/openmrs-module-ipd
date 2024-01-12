de76398f3b2baf4d38ddc88a4acc7d477204a7c2
Kalaiyarasan Raja <kraja@investvoyager.com>
2024-01-12 10:54:22 +0530

    Removed unwanted import

--- omod/src/main/java/org/openmrs/module/ipd/contract/MedicationAdministrationResponse.java
+++ omod/src/main/java/org/openmrs/module/ipd/contract/MedicationAdministrationResponse.java
@@ -4,7 +4,6 @@ import lombok.*;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.ipd.api.model.MedicationAdministrationNote;
 import org.openmrs.module.ipd.api.model.MedicationAdministrationPerformer;
-import org.openmrs.module.ipd.api.util.DateTimeUtil;
 import org.openmrs.module.webservices.rest.web.ConversionUtil;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
 
