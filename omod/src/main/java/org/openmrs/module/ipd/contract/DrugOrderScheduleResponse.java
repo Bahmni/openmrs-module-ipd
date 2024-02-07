d46a8357a494bd4243233446f7aa8a03c5e90ae6
Kalaiyarasan Raja <kraja@investvoyager.com>
2024-02-07 13:34:13 +0530

    pendingSlotsAvailable Key added to know whether any pending slots is available for drug Order

--- omod/src/main/java/org/openmrs/module/ipd/contract/DrugOrderScheduleResponse.java
+++ omod/src/main/java/org/openmrs/module/ipd/contract/DrugOrderScheduleResponse.java
@@ -1,6 +1,7 @@
 package org.openmrs.module.ipd.contract;
 
 import lombok.*;
+import org.openmrs.module.ipd.api.model.Slot;
 import org.openmrs.module.ipd.model.DrugOrderSchedule;
 
 import java.util.List;
@@ -16,6 +17,7 @@ public class DrugOrderScheduleResponse {
     private List<Long> remainingDaySlotsStartTime;
     private Long slotStartTime;
     private Boolean medicationAdministrationStarted;
+    private Boolean pendingSlotsAvailable;
     private String notes;
 
     public static DrugOrderScheduleResponse createFrom(DrugOrderSchedule drugOrderSchedule){
@@ -25,6 +27,7 @@ public class DrugOrderScheduleResponse {
                 remainingDaySlotsStartTime(drugOrderSchedule.getRemainingDaySlotsStartTime()).
                 slotStartTime(drugOrderSchedule.getSlotStartTime()).
                 medicationAdministrationStarted(drugOrderSchedule.getSlots().stream().anyMatch(slot -> slot.getMedicationAdministration() != null)).
+                pendingSlotsAvailable(drugOrderSchedule.getSlots().stream().anyMatch(slot -> slot.getStatus().equals(Slot.SlotStatus.SCHEDULED))).
                 notes(drugOrderSchedule.getSlots().get(0).getNotes()).
                 build();
     }
