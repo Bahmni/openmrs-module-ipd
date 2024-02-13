55f4bd963f6456500b64847a6a7b4d727c9723e3
Kalaiyarasan Raja <kraja@investvoyager.com>
2024-02-13 16:54:13 +0530

    Summary API Refactor changes

--- api/src/main/java/org/openmrs/module/ipd/api/model/WardPatientsSummary.java
+++ api/src/main/java/org/openmrs/module/ipd/api/model/WardPatientsSummary.java
@@ -1,10 +1,12 @@
 package org.openmrs.module.ipd.api.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Setter
 @Getter
 public class PatientStats {
