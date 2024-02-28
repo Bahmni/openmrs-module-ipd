27781c801a0b617695556953b6e47552a450b874
Kalaiyarasan Raja <kraja@investvoyager.com>
2024-02-28 15:24:59 +0530

    Add boolean key to show if all slots are attended

--- omod/src/main/java/org/openmrs/module/ipd/contract/DrugOrderScheduleResponse.java
+++ omod/src/main/java/org/openmrs/module/ipd/contract/DrugOrderScheduleResponse.java
@@ -19,6 +19,7 @@ public class DrugOrderScheduleResponse {
     private Long slotStartTime;
     private Boolean medicationAdministrationStarted;
     private Boolean pendingSlotsAvailable;
+    private Boolean allSlotsAttended;
     private String notes;
 
     public static DrugOrderScheduleResponse createFrom(DrugOrderSchedule drugOrderSchedule){
@@ -28,6 +29,7 @@ public class DrugOrderScheduleResponse {
                 remainingDaySlotsStartTime(drugOrderSchedule.getRemainingDaySlotsStartTime()).
                 slotStartTime(drugOrderSchedule.getSlotStartTime()).
                 medicationAdministrationStarted(drugOrderSchedule.getSlots().stream().anyMatch(slot -> slot.getMedicationAdministration() != null)).
+                allSlotsAttended(!(drugOrderSchedule.getSlots().stream().anyMatch(slot -> slot.getStatus().equals(Slot.SlotStatus.SCHEDULED)))).
                 pendingSlotsAvailable(drugOrderSchedule.getSlots().stream().anyMatch(slot -> (LocalDateTime.now().isBefore(slot.getStartDateTime()) && slot.getStatus().equals(Slot.SlotStatus.SCHEDULED)))).
                 notes(drugOrderSchedule.getSlots().get(0).getNotes()).
                 build();
