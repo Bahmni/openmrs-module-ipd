66b96deab3fd88c148877df6ca97276e0229b2b2
Umair Fayaz <omayrfayaz@gmail.com>
2024-04-09 18:52:23 +0530

    Gets rollover tasks from fhir task service using task names and status

--- api/src/main/java/org/openmrs/module/ipd/api/events/handler/impl/RolloverTaskEventHandler.java
+++ api/src/main/java/org/openmrs/module/ipd/api/events/handler/impl/RolloverTaskEventHandler.java
@@ -1,19 +1,27 @@
 package org.openmrs.module.ipd.api.events.handler.impl;
 
-import org.openmrs.module.ipd.api.events.model.ConfigDetail;
+import org.openmrs.module.fhirExtension.service.TaskService;
 import org.openmrs.module.ipd.api.events.ConfigLoader;
-import org.openmrs.module.ipd.api.events.model.IPDEvent;
 import org.openmrs.module.ipd.api.events.handler.IPDEventHandler;
+import org.openmrs.module.ipd.api.events.model.ConfigDetail;
+import org.openmrs.module.ipd.api.events.model.TaskDetail;
+import org.openmrs.module.ipd.api.events.model.IPDEvent;
+import org.openmrs.module.fhirExtension.model.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class RolloverTaskEventHandler implements IPDEventHandler {
 
     @Autowired
     ConfigLoader configLoader;
+
+    private TaskService taskService;
+
+    private static final String TASK_STATUS = "REQUESTED";
 
     @Override
     public void handleEvent(IPDEvent event) {
@@ -24,6 +32,16 @@ public class RolloverTaskEventHandler implements IPDEventHandler {
                 .orElse(null);
         System.out.println("eventConfig type RolloverTaskEventHandler " + eventConfig.getType());
         System.out.println("eventConfig tasks RolloverTaskEventHandler " + eventConfig.getTasks() + " size --- " + eventConfig.getTasks().size());
-        //create task based on configuration
+
+        List<String> taskNames = eventConfig.getTasks().stream()
+                .map(TaskDetail::getName)
+                .collect(Collectors.toList());
+
+        List<Task> rolloverTasks = taskService.getTasksByNameAndStatus(taskNames, TASK_STATUS);
+
+        for (Task task : rolloverTasks) {
+            System.out.println("Task : " + task.getFhirTask());
+
+        }
     }
 }
