abcdb78c1c16101ea68cc80bd8b38cfb224f7e0a
Arjun G <91885483+Arjun-Go@users.noreply.github.com>
2024-06-19 11:04:09 +0530

    A-1207053820490648 | [CURE IPD Bug] - Patients are not arranged in ascending order of bed number (#62)
    * add. sort logic to handle sorting of bed numbers that are purely integers
    
    * update. sort logic for numeric bed numbers

--- omod/src/main/java/org/openmrs/module/ipd/service/impl/IPDWardServiceImpl.java
+++ omod/src/main/java/org/openmrs/module/ipd/service/impl/IPDWardServiceImpl.java
@@ -1,6 +1,5 @@
 package org.openmrs.module.ipd.service.impl;
 
-
 import org.openmrs.module.ipd.api.model.AdmittedPatient;
 import org.openmrs.module.ipd.api.model.IPDPatientDetails;
 import org.openmrs.module.ipd.api.model.WardPatientsSummary;
@@ -12,6 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 @Service
 @Transactional
@@ -24,6 +25,7 @@ public class IPDWardServiceImpl implements IPDWardService {
         this.wardService = wardService;
     }
 
+    private static final Pattern numericPattern = Pattern.compile("^\\d+$");
 
     @Override
     public WardPatientsSummary getIPDWardPatientSummary(String wardUuid, String providerUuid) {
@@ -38,11 +40,12 @@ public class IPDWardServiceImpl implements IPDWardService {
         if (admittedPatients ==null ){
             return new IPDPatientDetails(new ArrayList<>(),0);
         }
+        List<AdmittedPatient> admittedPatientsSortedList = Objects.equals(sortBy, "bedNumber") ? sortNumericBedNumbers(admittedPatients) : admittedPatients;
 
-        offset = Math.min(offset, admittedPatients.size());
-        limit = Math.min(limit, admittedPatients.size() - offset);
+        offset = Math.min(offset, admittedPatientsSortedList.size());
+        limit = Math.min(limit, admittedPatientsSortedList.size() - offset);
 
-        return new IPDPatientDetails(admittedPatients.subList(offset, offset + limit), admittedPatients.size());
+        return new IPDPatientDetails(admittedPatientsSortedList.subList(offset, offset + limit), admittedPatientsSortedList.size());
     }
 
     @Override
@@ -53,11 +56,12 @@ public class IPDWardServiceImpl implements IPDWardService {
         if (admittedPatients ==null ){
             return new IPDPatientDetails(new ArrayList<>(),0);
         }
+        List<AdmittedPatient> admittedPatientsSortedList = Objects.equals(sortBy, "bedNumber") ? sortNumericBedNumbers(admittedPatients) : admittedPatients;
 
-        offset = Math.min(offset, admittedPatients.size());
-        limit = Math.min(limit, admittedPatients.size() - offset);
+        offset = Math.min(offset, admittedPatientsSortedList.size());
+        limit = Math.min(limit, admittedPatientsSortedList.size() - offset);
 
-        return new IPDPatientDetails(admittedPatients.subList(offset, offset + limit), admittedPatients.size());
+        return new IPDPatientDetails(admittedPatientsSortedList.subList(offset, offset + limit), admittedPatientsSortedList.size());
     }
 
     @Override
@@ -65,13 +69,33 @@ public class IPDWardServiceImpl implements IPDWardService {
                                                      Integer offset, Integer limit, String sortBy) {
 
         List<AdmittedPatient> admittedPatients = wardService.searchWardPatients(wardUuid,searchKeys,searchValue,sortBy);
+
         if (admittedPatients ==null ){
             return new IPDPatientDetails(new ArrayList<>(),0);
         }
 
-        offset = Math.min(offset, admittedPatients.size());
-        limit = Math.min(limit, admittedPatients.size() - offset);
+        List<AdmittedPatient> admittedPatientsSortedList = Objects.equals(sortBy, "bedNumber") ? sortNumericBedNumbers(admittedPatients) : admittedPatients;
 
-        return new IPDPatientDetails(admittedPatients.subList(offset, offset + limit), admittedPatients.size());
+        offset = Math.min(offset, admittedPatientsSortedList.size());
+        limit = Math.min(limit, admittedPatientsSortedList.size() - offset);
+
+        return new IPDPatientDetails(admittedPatientsSortedList.subList(offset, offset + limit), admittedPatientsSortedList.size());
+    }
+
+    private List<AdmittedPatient> sortNumericBedNumbers(List<AdmittedPatient> admittedPatients) {
+
+        boolean allNumeric = admittedPatients.stream()
+                .map(patient -> patient.getBedPatientAssignment().getBed().getBedNumber())
+                .allMatch(bedNumber -> numericPattern.matcher(bedNumber).matches());
+
+        if(allNumeric) {
+            admittedPatients.sort((patientA, patientB) -> {
+                String bedNumberA = patientA.getBedPatientAssignment().getBed().getBedNumber();
+                String bedNumberB = patientB.getBedPatientAssignment().getBed().getBedNumber();
+
+                return Integer.compare(Integer.parseInt(bedNumberA), Integer.parseInt(bedNumberB));
+            });
+        }
+        return admittedPatients;
     }
 }
